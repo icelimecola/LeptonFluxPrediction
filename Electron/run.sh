9dfca62521ff3cf883ddbdd8c953f5e730cf92e0
madeleine <2198248606@qq.com>
2026-06-30 19:44:59 +0800

    lfp-ele: jsub sh: move subsh into certain folder

--- Electron/run.sh
+++ Electron/run.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-mkdir -p JSUB/error JSUB/output Model Figure/lstm
+mkdir -p jobs JSUB/error JSUB/output Model Figure/lstm
 
 for i in 0 1 2 3 4 5 6 7; do
-  sed "s/NUM/$i/g" jsub_lstm_elec.sh > jsub_lstm_elec_$i.sh
-  chmod +x jsub_lstm_elec_$i.sh
-  jsub < jsub_lstm_elec_$i.sh
+  sed "s/NUM/$i/g" jsub_lstm_elec.sh > JSUBs/jsub_lstm_elec_$i.sh
+  chmod +x JSUBs/jsub_lstm_elec_$i.sh
+  jsub < JSUBs/jsub_lstm_elec_$i.sh
   echo "Submitted paras_$i.yaml"
 done
--- Electron/run_draw.sh
+++ Electron/run_draw.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-mkdir -p JSUB/error JSUB/output Data/lstm2 Figure/lstm2
+mkdir -p jobs JSUB/error JSUB/output Data/lstm2 Figure/lstm2
 
-sed "s/NUM/0/g" jsub_draw.sh > jsub_draw_0.sh
-chmod +x jsub_draw_0.sh
-jsub < jsub_draw_0.sh
+sed "s/NUM/0/g" jsub_draw.sh > JSUBs/jsub_draw_0.sh
+chmod +x JSUBs/jsub_draw_0.sh
+jsub < JSUBs/jsub_draw_0.sh
 echo "Submitted draw job"
